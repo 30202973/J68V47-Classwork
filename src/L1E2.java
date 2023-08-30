69741447363e4222d022caefbb2ab54bcdc8d8ba
30202973 <30202973@nescol.ac.uk>
2023-08-30 16:16:21 +0100

    White flakes falling down Blanketing the ground in white Winter's cold embrace

---
+++ src/L1E2.java
@@ -0,0 +1,19 @@
+
+public class L1E2 {
+    public static void main(String[] args) {
+        String line1 = "Here are 5-9 letter anagrams:";
+        String line2 = "heirs jack = HIJACKERS";
+        String line3 = "etch quine = TECHNIQUE";
+        String line4 = "gp unglued = UNPLUGGED";
+        String line5 = "acquit edt = ACQUITTED";
+        String line6 = "Arno basis = ABRASIONS";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+    }
+
+    }
