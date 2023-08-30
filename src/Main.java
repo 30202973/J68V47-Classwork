a0506cc2b35070fc9ebd2f3269775ad66c3997e9
30202973 <30202973@nescol.ac.uk>
2023-08-30 15:39:58 +0100

    White flakes falling down Blanketing the ground in white Winter's cold embrace

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        String line1 = "White flakes falling down";
+        String line2 = "Blanketing the ground in white";
+        String line3 = "Winter's cold embrace";
 
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
     }
 }
