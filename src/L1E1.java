0c4fe19dfbe0c4a1455bb1213b0bc6bfe6f34614
30202973 <30202973@nescol.ac.uk>
2023-08-30 15:46:34 +0100

    White flakes falling down Blanketing the ground in white Winter's cold embrace

---
+++ src/L1E1.java
@@ -0,0 +1,11 @@
+public class L1E1 {
+    public static void main(String[] args) {
+        String line1 = "White flakes falling down";
+        String line2 = "Blanketing the ground in white";
+        String line3 = "Winter's cold embrace";
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        String line1 = "White flakes falling down";
-        String line2 = "Blanketing the ground in white";
-        String line3 = "Winter's cold embrace";
-
-        System.out.println(line1);
-        System.out.println(line2);
-        System.out.println(line3);
+        System.out.println("Hello world!");
     }
 }
