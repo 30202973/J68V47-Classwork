71bca6a2eeb10d5f843f0cddb5908b0f825e155f
30202973 <30202973@nescol.ac.uk>
2023-09-20 14:42:20 +0100

    Viva una y otra vez!

--- src/L4E2.java
+++ src/L4E2.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class L4E2 {
+import java.util.*;
+public class L4E2 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Q1. What is the capital of France? ");
+        String q1 = input.nextLine();
+        if (q1.toLowerCase().equals("PARIS")) {?
+        }else {
+            System.out.println("Sorry, the correct answer is PARIS.");
+        }
+
+        System.out.print("Q2. What is the capital of Senegal? ");
+        String q2 = input.nextLine();
+        if (q2.toLowerCase().equals("DAKAR")){?
+        }else {
+            System.out.println("Sorry, the correct answer is DAKAR.");
+        }
+
+        System.out.print("Q3. What is the capital of South Korea? ");
+        String q3 = input.nextLine();
+        if (q3.toLowerCase().equals("SEOUL")) {?
+        }else {
+            System.out.println("Sorry, the correct answer is SEOUL.");
+        }
+
+    }
+
 }
--- src/L4E3.java
+++ src/L4E3.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class L4E3 {
+import java.util.*;
+public class L4E3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your total purchase amount: £");
+        double total = input.nextDouble();
+
+
+    }
 }
