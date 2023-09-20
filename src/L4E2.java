7037ab8fb91dd35bcdc9dd9a3c962805936c5f6a
30202973 <30202973@nescol.ac.uk>
2023-09-20 14:45:30 +0100

    Viva una y otra vez!

--- src/L4E2.java
+++ src/L4E2.java
@@ -6,23 +6,20 @@ public class L4E2 {
 
         System.out.print("Q1. What is the capital of France? ");
         String q1 = input.nextLine();
-        if (q1.toLowerCase().equals("PARIS")) {?
-        }else {
-            System.out.println("Sorry, the correct answer is PARIS.");
+        if (!q1.toLowerCase().equals("paris")) {
+           System.out.println("Sorry, the correct answer is PARIS.");
         }
 
         System.out.print("Q2. What is the capital of Senegal? ");
         String q2 = input.nextLine();
-        if (q2.toLowerCase().equals("DAKAR")){?
-        }else {
-            System.out.println("Sorry, the correct answer is DAKAR.");
+        if (!q2.toLowerCase().equals("dakar")){
+           System.out.println("Sorry, the correct answer is DAKAR.");
         }
 
         System.out.print("Q3. What is the capital of South Korea? ");
         String q3 = input.nextLine();
-        if (q3.toLowerCase().equals("SEOUL")) {?
-        }else {
-            System.out.println("Sorry, the correct answer is SEOUL.");
+        if (!q3.toLowerCase().equals("seoul")) {
+           System.out.println("Sorry, the correct answer is SEOUL.");
         }
 
     }
