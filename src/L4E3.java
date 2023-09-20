0f8ac0d41d2d775e6b330347ade2e6cee636a890
30202973 <30202973@nescol.ac.uk>
2023-09-20 15:23:29 +0100

    Ship shape shopper shipping

--- src/L4E3.java
+++ src/L4E3.java
@@ -4,6 +4,14 @@ public class L4E3 {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter your total purchase amount: £");
         double total = input.nextDouble();
+        if (total < 50) {
+            System.out.println("Shipping cost is = £10.00" );
+            System.out.format("Your final total is = £" + (total+10));
+        } else {
+            System.out.println("Your final total is = £" + total);
+        }
+
+
 
 
     }
