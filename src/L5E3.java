20a6306c2eda9dadd6f9e63dbf4161aaf084438a
30202973 <30202973@nescol.ac.uk>
2023-09-27 15:36:41 +0100

    Quiz game

--- src/L5E3.java
+++ src/L5E3.java
@@ -2,5 +2,21 @@ import java.util.*;
 public class L5E3 {
     public static void main(String[] args) {
 
+        int score = 0;
+
+        for( int count=1; count<=10; count++ ) {
+            System.out.print("Q1. What is 2 x 3 ? ");
+            Scanner input = new Scanner(System.in);
+            int q1 = input.nextInt();
+            if (q1 != 6) {
+                System.out.println("Sorry, the correct answer is 6.");
+            } else {
+                score++;
+            }
+        }
+        System.out.println("Score is: " + score);
+
+
+
     }
 }
