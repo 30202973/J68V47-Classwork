527c40c7dd8b8dd83b1ba04f2cf6936d669f9d73
30202973 <30202973@nescol.ac.uk>
2023-09-27 14:26:18 +0100

    Count down

--- src/L5E2.java
+++ src/L5E2.java
@@ -1,5 +1,13 @@
 
 import java.util.*;
 public class L5E2 {
+    public static void main(String[] args) {
 
+        for( int count=10; count>=1; count-=1 )
+        {
+            System.out.println(count);
+        }
+        System.out.println("Blastoff!!!!!!!");
+
+    }
 }
