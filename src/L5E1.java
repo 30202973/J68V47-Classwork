99012f0d1f3b23f1f8d3aab97d4d013ac83a397e
30202973 <30202973@nescol.ac.uk>
2023-09-27 14:21:26 +0100

    Times table program

--- src/L5E1.java
+++ src/L5E1.java
@@ -2,11 +2,12 @@ import java.util.*;
 public class L5E1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Input abase number:");
+        System.out.print("Input a base number: ");
+        int base = input.nextInt();
 
-        for( int count=10; count<=60; count+=5 )
+        for( int count=1; count<=12; count++ )
         {
-            System.out.println(count);
+            System.out.println(base + " x " + count + " = " + (count*base));
         }
 
     }
