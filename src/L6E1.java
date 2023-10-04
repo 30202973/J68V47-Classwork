a469a3d22be5b0056f5d14361e2e3bbbd5acd02d
30202973 <30202973@nescol.ac.uk>
2023-10-04 13:57:30 +0100

    while loop

---
+++ src/L6E1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class L6E1 {
+    public static void main(String[] args) {
+        int count = 10;
+        while( count<=24 )
+        {
+            System.out.println(count);
+            count += 2;
+        }
+
+    }
+
+}
