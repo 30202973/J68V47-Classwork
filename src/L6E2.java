17d856f9822e43dcb989a11927fb89273388782e
30202973 <30202973@nescol.ac.uk>
2023-10-04 14:26:53 +0100

    menu driven program

--- src/L6E2.java
+++ src/L6E2.java
@@ -2,11 +2,12 @@ import java.util.*;
 public class L6E2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Options:" + "\n" + "1.Say “hello”"
-                + "\n" + "2.Tell me the time" + "\n" + "3.Tell me a joke" + "\n" + "4.Quit" + "\n");
 
         int option = 4;
         do{
+            System.out.println("Options:" + "\n" + "1.Say “hello”"
+                    + "\n" + "2.Tell me the time" + "\n" + "3.Tell me a joke" + "\n" + "4.Quit" + "\n");
+
             System.out.println("Choose an option:");
            option = input.nextInt();
 
--- src/L6E3.java
+++ src/L6E3.java
@@ -1,4 +1,23 @@
 import java.util.*;
 public class L6E3 {
+    public static void main(String[] args) {
+
+        int score = 0;
+
+        for( int count=1; count<=10; count++ ) {
+            System.out.print("Q1. What is 2 x 3 ? ");
+            Scanner input = new Scanner(System.in);
+            int q1 = input.nextInt();
+            if (q1 != 6) {
+                System.out.println("Sorry, the correct answer is 6.");
+            } else {
+                score++;
+            }
+        }
+        System.out.println("Score is: " + score);
+
+
+
+    }
 
 }
