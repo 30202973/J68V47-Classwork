3574e67d69a05d7ced803f73fdec011c47712406
30202973 <30202973@nescol.ac.uk>
2023-10-04 14:23:14 +0100

    menu driven program

--- src/L6E2.java
+++ src/L6E2.java
@@ -1,4 +1,18 @@
 import java.util.*;
 public class L6E2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Options:" + "\n" + "1.Say “hello”"
+                + "\n" + "2.Tell me the time" + "\n" + "3.Tell me a joke" + "\n" + "4.Quit" + "\n");
+
+        int option = 4;
+        do{
+            System.out.println("Choose an option:");
+           option = input.nextInt();
+
+        }
+        while( option!=4 );
+
+    }
 
 }
