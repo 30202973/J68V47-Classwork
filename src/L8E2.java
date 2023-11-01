76f4143cc630d3ccf1c9757a2886446d790b5fb2
30202973 <30202973@nescol.ac.uk>
2023-11-01 14:19:14 +0000

    Exam Scores

--- src/L8E2.java
+++ src/L8E2.java
@@ -1,4 +1,11 @@
 import java.util.*;
 public class L8E2 {
+    public static void main(String[] args) {
+        int[] scores = {35,67,80,13,73};
+        for (int index  = 0; index  < scores.length; index ++) {
+            System.out.println( scores[index ] );
+        }
+        System.out.println("The average exam score is: " + ((scores[0] + scores[1] + scores[2] + scores[3] + scores[4]) / 5));
 
+    }
 }
