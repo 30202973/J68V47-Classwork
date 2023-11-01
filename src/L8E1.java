36398caa91ad8b7b46591453a7b17b5c23e9e43f
30202973 <30202973@nescol.ac.uk>
2023-11-01 14:07:34 +0000

    Storing Student Names

---
+++ src/L8E1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class L8E1 {
+    public static void main(String[] args) {
+        String[] students = {"Wiktoria", "Amanda", "Evelyn", "Ash", "Amina"};
+        System.out.println("The first student's name is: " + students[0]);
+        System.out.println("The last student's name is: " + students[students.length-1]);
+        System.out.println("The number of items in the array is: " + students.length );
+
+    }
+}
---
+++ src/L8E3.java
@@ -0,0 +1,4 @@
+import java.util.*;
+public class L8E3 {
+
+}
