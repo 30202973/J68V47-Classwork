d6a69860bec81aaf2fe2afa1c4d20ebfd93f7954
30202973 <30202973@nescol.ac.uk>
2023-11-27 10:21:22 +0000

    Subtotal

--- src/L11E1.java
+++ src/L11E1.java
@@ -5,15 +5,15 @@ public class L11E1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); {
             System.out.print("Input a number: ");
-                int base = input.nextInt();
+            int base = input.nextInt();
 
-                try (PrintWriter out = new PrintWriter(new FileWriter("output.txt"))) {
-                    for (int count = 1; count <= 12; count++) {
-                        out.println(base + " x " + count + " = " + (count * base));
-                    }
-                } catch (IOException e) {
-                    System.out.println("Error occurred writing to file: " + e);
+            try (PrintWriter out = new PrintWriter(new FileWriter("output.txt"))) {
+                for (int count = 1; count <= 12; count++) {
+                    out.println(base + " x " + count + " = " + (count * base));
                 }
+            } catch (IOException e) {
+                System.out.println("Error occurred writing to file: " + e);
             }
         }
     }
+}
--- src/L11E4.java
+++ src/L11E4.java
@@ -1,2 +1,44 @@
+import java.io.*;
+
 public class L11E4 {
+
+    public static void main(String[] args) {
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(getFileName("Enter the first file name: ")));
+             BufferedReader reader2 = new BufferedReader(new FileReader(getFileName("Enter the second file name: ")))) {
+
+            boolean areIdentical = compareFileContents(reader1, reader2);
+
+            if (areIdentical) {
+                System.out.println("The contents of the files are identical.");
+            } else {
+                System.out.println("The contents of the files are not identical.");
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error occurred while reading the files: " + e);
+        }
+    }
+
+    private static String getFileName(String prompt) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(prompt);
+        return reader.readLine();
+    }
+
+    private static boolean compareFileContents(BufferedReader reader1, BufferedReader reader2) throws IOException {
+        String line1, line2;
+        while ((line1 = reader1.readLine()) != null) {
+            line2 = reader2.readLine();
+
+            if (line2 == null) {
+                return false;
+            }
+
+            if (!line1.equals(line2)) {
+                return false;
+            }
+        }
+
+        return reader2.readLine() == null;
+    }
 }
