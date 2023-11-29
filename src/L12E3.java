e2462cf87d1f45df3a4568f8762a7301b4e2093a
30202973 <30202973@nescol.ac.uk>
2023-11-29 15:35:35 +0000

    Building your own Java class

--- src/L12E3.java
+++ src/L12E3.java
@@ -1,2 +1,21 @@
 public class L12E3 {
+    public static void displayCarSales(carSales thiscarSales) {
+        System.out.println(thiscarSales.make + " " + thiscarSales.model + " " + "(" + thiscarSales.year + ")" + "\n" + thiscarSales.vin + "\n" + thiscarSales.colour  + "\n" + thiscarSales.mileage + "\n" + thiscarSales.condition + "\n" + thiscarSales.price);
+    }
+
+    public static void main(String[] args) {
+        carSales carSales1 = new carSales();
+        displayCarSales(carSales1);
+
+        carSales1.make = "Ferrari";
+        carSales1.model = "F430";
+        carSales1.year = 2009;
+        carSales1.vin = "3GYVKNEFXAG625569";
+        carSales1.colour = "White";
+        carSales1.mileage = 45336;
+        carSales1.condition = "Good";
+        carSales1.price = 55125;
+        displayCarSales(carSales1);
+
+    }
 }
---
+++ src/carSales.java
@@ -0,0 +1,11 @@
+public class carSales {
+    public String make = "";
+    public String model = "";
+    public int year = 0;
+    public String vin = "";
+    public String colour = "";
+    public int mileage = 0;
+    public String condition = "";
+    public int price = 0;
+
+}
