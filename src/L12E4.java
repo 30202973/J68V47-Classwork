eaf35328a793af7ddead9536a0d003a32abe8583
30202973 <30202973@nescol.ac.uk>
2023-12-04 10:20:40 +0000

    Load in car details from file and display in console

--- src/L12E4.java
+++ src/L12E4.java
@@ -1,49 +1,44 @@
 import java.util.*;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 public class L12E4 {
+
     public static ArrayList<carSales> loadFile(String filename) {
-        List<String> carSaleDetails;
-        ArrayList<carSales> thisCar = new ArrayList<>();
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(filename));
+        List<String> carDetails;
+        ArrayList<carSales> carList = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
             String line = in.readLine();
             while (line != null) {
-                carSaleDetails = Arrays.asList(line.split(","));
-                carSales newcarSales = new carSales();
-                newcarSales.make = carSaleDetails.get(0);
-                newcarSales.model = carSaleDetails.get(1);
-                newcarSales.year = Integer.parseInt(carSaleDetails.get(2));
-                newcarSales.vin = carSaleDetails.get(3);
-                newcarSales.colour = carSaleDetails.get(4);
-                newcarSales.mileage = Integer.parseInt(carSaleDetails.get(5));
-                newcarSales.condition = carSaleDetails.get(6);
-                newcarSales.price = Integer.parseInt(carSaleDetails.get(7));
+                carDetails = Arrays.asList(line.split(","));
+                carSales newCar = new carSales();
+                newCar.make = carDetails.get(0);
+                newCar.model = carDetails.get(1);
+                newCar.year = Integer.parseInt(carDetails.get(2));
+                newCar.vin = carDetails.get(3);
+                newCar.price = Integer.parseInt(carDetails.get(4));
+                newCar.colour = carDetails.get(5);
+                newCar.mileage = Integer.parseInt(carDetails.get(6));
+                newCar.condition = carDetails.get(7);
 
-                thisCar.add(newcarSales);
+                carList.add(newCar);
                 line = in.readLine();
             }
         } catch (IOException e) {
             System.out.println("Error occurred reading file: " + e.toString());
         }
-        return thisCar;
+        return carList;
     }
 
-
-
-
-    public static void displayCarSales(carSales thiscarSales) {
-        System.out.println(thiscarSales.make + " " + thiscarSales.model + " " + "(" + thiscarSales.year + ")" + "\n" + "Vin: " + thiscarSales.vin + "\n" + "Colour: " + thiscarSales.colour  + "\n" + "Mileage: " + thiscarSales.mileage + "\n" + "Condition: " + thiscarSales.condition + "\n" + "Price: " + thiscarSales.price + "\n" + "-----------------------------");
+    public static void displayCar(carSales car) {
+        System.out.println(car.make + " " + car.model + " " + "(" +car.year + ")" + "\n" + "Vin: " + car.vin + "\n" + "Colour: " + car.colour + "\n" + "Mileage: " + car.mileage + "\n" + "Condition: " + car.condition + "\n" + "Price: "+ car.price + "\n" + "------------------------------------------");
     }
 
     public static void main(String[] args) {
-
-        ArrayList<carSales> thisCar = loadFile("src/carSales.txt");
-
-        for (carSales thiscarSales : thisCar) {
-            displayCarSales(thiscarSales);
+        ArrayList<carSales> carList = loadFile("src/carSales.txt");
+        for (carSales carSales : carList) {
+            displayCar(carSales);
         }
     }
 
